a40594aa645158a70e9d19e3a9e430b62892f688
TamoshaytisV <amd.vladas@gmail.com>
2020-05-06 22:00:21 +0300

    revert unknown char in comment

--- hollidays/pages/base/elements.py
+++ hollidays/pages/base/elements.py
@@ -81,7 +81,7 @@ class InputElement(BaseElement):
 
             return element.attrs("value")[0] == value
 
-        # ждем пока не выполнится функция˚
+        # ждем пока не выполнится функция
         # промис ожидает, что функция вернет какое-то значение
         EmptyPromise(execute, "InputElement value is updated.").fulfill()
 
